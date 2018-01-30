97436893eb2cdacece6f398603b2ab7d87a5a50a
Juan Estrada <juansebastianestrada@gmail.com>
2018-01-30 11:05:25 -0400

    fixing calendar

--- report.py
+++ report.py
@@ -1,7 +1,9 @@
 from datetime import datetime, date, time, timedelta
 import calendar
 print calendar.isleap(1900)
-
+from reportlab.lib import colors
+from reportlab.lib.pagesizes import letter, inch
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
 
 def getMaxDaysInMonth(datetime):
     if datetime.month !=2:
@@ -25,16 +27,56 @@ def getMaxDaysInMonth(datetime):
             return 28
             
 
+def returnDateObject(year):
+    return {
+        'year':year,
+        1:{'week1':[],'week2':[] },
+        2:{'week1':[],'week2':[] },
+        3:{'week1':[],'week2':[] },
+        4:{'week1':[],'week2':[] },
+        5:{'week1':[],'week2':[] },
+        6:{'week1':[],'week2':[] },
+        7:{'week1':[],'week2':[] },
+        8:{'week1':[],'week2':[] },
+        9:{'week1':[],'week2':[] },
+        10:{'week1':[],'week2':[] },
+        11:{'week1':[],'week2':[] },
+        12:{'week1':[],'week2':[] }
+    }
+
+
+def appendToDateObject(time,arrayDateRange):
+    index = 0
+    print len(arrayDateRange)
+    for i in range(0,len(arrayDateRange)):
+        if(arrayDateRange[i]['year'] == time.year):
+            index = i
+    if (time.day<15):
+        arrayDateRange[index][time.month]['week1'].append(time)
+    if (time.day>=15):
+        arrayDateRange[index][time.month]['week2'].append(time)
+    return arrayDateRange
+
 
 def genCalendar(time1,time2):
-    days = time2-time1
-    print days.days
+    dateRange = []
+    timeDelta = time2 - time1
+    for i in range(time1.year,time2.year + 1 ,1):
+        print (i, time1.year, time2.year)
+        dateRange.append ( returnDateObject(i) )
+
+
+    for z in range(0,timeDelta.days + 1):
+        toAppend = time1 + timedelta(days=z)
+        dateRange = appendToDateObject(toAppend,dateRange)
+    
+    print dateRange
+
+
+
 
 
 def getDays(time1,time2):
     dt1 = datetime.strptime(time1, "%Y-%m-%d")
     dt2 = datetime.strptime(time2, "%Y-%m-%d")
-    print getMaxDaysInMonth(dt1)
-    print getMaxDaysInMonth(dt2)
-    print (dt1,dt2)
     genCalendar(dt1,dt2)
